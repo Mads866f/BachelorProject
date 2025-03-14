fec8c46bfe885a1ecf90b63698bcbf873fe87498
mads866f <mads.larsen@boostplm.com>
2025-03-14 12:04:54 +0100

    [ADDED] Last part of the connection to the database

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,13 @@
+using System.Data;
 using Backend;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Get Connection string
-var connectionString = builder.Configuration.GetConnectionString("De")
+var connectionString = builder.Configuration.GetConnectionString("Postgres_db");
+
+builder.Services.AddSingleton<IDbConnection>(sp => new NpgsqlConnection(connectionString));
 
 
 // Add services to the container.
@@ -24,6 +28,28 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+// Ensure database and tables are set up
+void EnsureDatabaseSetup(IDbConnection db)
+{
+    db.Open();
+    using var cmd = db.CreateCommand();
+    cmd.CommandText = @"
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        )";
+    cmd.ExecuteNonQuery();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<IDbConnection>();
+    Console.WriteLine("IM AM HERE");
+    EnsureDatabaseSetup(db);
+}
+
+app.MapGet("/", () => "Dapper API is running!");
 
 app.UseHttpsRedirection();
 
