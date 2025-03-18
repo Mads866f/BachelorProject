d3a03696a0f6328c88a69c4d8170d76ede6d8e24
k-b-e-m <202205825@post.au.dk>
2025-03-18 14:38:56 +0100

    Drowdown works

--- Front/Program.cs
+++ Front/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
 builder.Services.AddMudServices();
 
 builder.AddConfiguration();
