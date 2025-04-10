3ebfa3e48dab15416387c04682f220dd17006cf7
mads866f <mads.larsen@boostplm.com>
2025-04-10 10:44:18 +0200

    [ADDED] DTO FOR ELECTIONRESULTS

---
+++ DTO/Models/ElectionResult.cs
@@ -0,0 +1,21 @@
+namespace DTO.Models;
+
+/// <summary>
+/// DTO Representing the result of a election
+/// </summary>
+public class ElectionResult
+{
+    /// <summary>
+    /// The Unique identifier of the result calculation
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// The unique identifier of the associated election
+    /// </summary>
+    public Guid ElectionId { get; set; }
+    public List<Project> SubmittedProjects { get; set; } = [];
+    public List<Project> ElectedProjects { get; set; } = [];
+    public string UsedMethod { get; set; } = "";
+    public string UsedBallot { get; set; } = "";
+
+}
